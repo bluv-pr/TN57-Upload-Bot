94979c1a79259c3924ed271ea2f1a1f7558da097
Balaji Shinnosuke <62749324+bluv-pr@users.noreply.github.com>
2021-04-25 12:51:32 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,18 @@
 class Translation(object):
     START_TEXT = """Hi {},
-I'm X-URL Uploader!
+I'm <b>TN57 URL UPLOAD BOT</b>!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
-/help for more details!"""
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+/help for more details!
+<b>Made By @TN57_BotZ</b>"""
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo To Me...😇\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "Now Downloading.."
-    UPLOAD_START = "Now Uploading.."
+    DOWNLOAD_START = "Downloading Your File To My Server...💾"
+    UPLOAD_START = "Uploading To Tg...😼"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @xurluploaderbot)"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using Me [Share Me](https://telegram.me/share/url?url=https://telegram.me/TN57_Upload_Bot))"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@TN57_BotZ 😼"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -24,13 +25,23 @@ URL | filename | username | password"""
    SVideo - Give File as video with Screenshots
    DFile  - Give File (video) as file with Screenshots
    Video  - Give File as video without Screenshots
-   File   - Give File without Screenshots
-
-If bot didn't respond, contact @xgorn"""
+   File   - Give File without Screenshots.
+   
+Support Group : @TN57_BotZSupport 🤠"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
 """
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    CANCEL_STR = "Process Cancelled!!!🤧"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds \n[shareme](https://telegram.me/share/url?url=https://telegram.me/TN57_Upload_Bot)"
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    ABOUT_TEXT = """
+<b>😇Mʏ Nᴀᴍᴇ :</b> <a href='https://t.me/TN57_Upload_Bot'>Tɴ57 Uʀʟ Uᴘʟᴏᴀᴅ Bᴏᴛ</a>\n
+<b>🤠Mʏ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ :</b> <a href='https://t.me/TN57_BotZ'>Tɴ57 Bᴏᴛᴢ</a>\n
+<b>ℹ️Vᴇʀsɪᴏɴ :</b> <a href='https://t.me/TN57_Upload_Bot'>0.9.2 Bᴇᴛᴀ</a>\n
+<b>🤩Lᴇᴇᴄʜ Gʀᴏᴜᴘ :</b> <a href='https://t.me/TN57_Leech'>Cʟɪᴄᴋ Hᴇʀᴇ</a>\n
+<b>🤒Sᴇʀᴠᴇʀ :</b> <a href='https://heroku.com'>Hᴇʀᴏᴋᴜ</a>\n
+<b>😉Lɪʙʀᴀʀʏ :</b> <a href='https://github.com/pyrogram'>Pʏʀᴏɢʀᴀᴍ 1.2.8</a>\n
+<b>🐍Lᴀɴɢᴜᴀɢᴇ :</b> <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3.9.4</a>\n
+<b>😴Dᴇᴠᴇʟᴏᴘᴇʀ :</b> <a href='https://t.me/BLuVDS'>✯ °•Bᴀʟᴀᴊɪ•° ✯</a>\n
+<b>Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ :</b> <a href='https://t.me/TN57_BotzSupport'>Cʟɪᴄᴋ Hᴇʀᴇ</a>\n"""
